016b44704e6112391b3f30c3e36d2cd6232aaf2f
LordCracken <vladislav070201@gmail.com>
2023-03-28 17:44:53 +0300

    convert JS-scripts into JS-modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import roads from './roads.json' assert { type: 'json' };
 
-import VillageState from './scripts/village';
-import buildGraph from './scripts/buildGraph';
+import VillageState from './scripts/village.mjs';
+import buildGraph from './util/buildGraph.mjs';
 
 const roadGraph = buildGraph(roads);
 const village = VillageState.random(roadGraph);
